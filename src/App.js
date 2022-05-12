ae5a34ca7649faa9c3a0d80e8de95de1bd25fb61
Billmetal <billmetal9@gmail.com>
2022-05-12 10:31:19 -0300

    Implementado um global style com styled-component .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { StylesProvider } from "@material-ui/styles";
+import { CssBaseline } from "../node_modules/@material-ui/core/index";
+import GlobalStyle from "../../pwa-covid19/src/commons/styles/global-style";
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <StylesProvider injectFirst>
+      <CssBaseline/>
+      <GlobalStyle/>
+      <div>
+        Teste
+      </div>
+    </StylesProvider>
   );
 }
 
---
+++ src/commons/styles/global-style.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "../../../node_modules/styled-components/dist/styled-components.cjs";
+import CovidImg from "../../assets/images/covid.jpg";
+
+const globalStyle = createGlobalStyle`
+    * {
+        outline: none;
+        box-sizing: border-box;
+    }
+
+    body {
+        line-height: normal;
+    }
+
+    html, body {
+        width: 100%;
+        min-height: 100%;
+        display: flex;
+        padding: 0;
+        margin: 0;
+    }
+
+    #root {
+        background: url(${CovidImg});
+        width: 100%;
+        height: 100%;
+        background-size: cover;
+        background-position: center center;
+    }
+`
+
+export default globalStyle;
