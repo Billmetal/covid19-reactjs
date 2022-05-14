f628880a1e6cfb7065b8a7eb3b3b262a674bde23
Billmetal <billmetal9@gmail.com>
2022-05-13 23:09:14 -0300

    Montado os componentes e utilizando dados da api .

---
+++ src/components/Button.jsx
@@ -0,0 +1,3 @@
+import Button from "../../node_modules/@material-ui/core/Button";
+
+export default Button;
---
+++ src/components/Grid.jsx
@@ -0,0 +1,4 @@
+import Grid from "../../node_modules/@material-ui/core/Grid";
+
+export default Grid;
+
